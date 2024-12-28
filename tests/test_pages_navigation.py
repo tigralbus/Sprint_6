28f673e25ce6911e4a1c233fb4eb160d7e8d180f
Anastasiia Dotsina <tigralbus@yandex.ru>
2024-12-28 13:41:57 +0300

    Исправлено замечание: Повторный коммент: Нужно исправить: здесь и далее: условий в тестах быть не может, тест всегда однозначен. Если это не вписывается в параметризацию, значит ее быть не должно и это просто несколько отдельных тестовых методов

--- tests/test_pages_navigation.py
+++ tests/test_pages_navigation.py
@@ -1,5 +1,4 @@
 import allure
-import pytest
 
 from conftest import driver
 from constants import Constants
@@ -9,53 +8,69 @@ from pages.order_page_who import OrderPageWho
 
 
 class TestPagesNavigation:
-    # переход по ссылке Дзена на разных этапах заказа
-    @pytest.mark.parametrize('depth', [0, 1, 2])
-    @allure.title('Проверка перехода со страниц Самоката на Яндекс Дзен.')
-    def test_redirect_to_ya_dzen(self, driver, depth):
+    @allure.title('Проверка перехода с главной страницы Самоката на Яндекс Дзен.')
+    def test_redirect_to_ya_dzen_from_main_page(self, driver):
+        page = self.open_home_page(driver)
+        self.assert_redirect_to_yandex(page, driver)
+
+    @allure.title('Проверка перехода со страницы ввода персональных данных заказа на Яндекс Дзен.')
+    def test_redirect_to_ya_dzen_from_order_who_page(self, driver):
+        page = self.open_who_page(driver)
+        self.assert_redirect_to_yandex(page, driver)
+
+    @allure.title('Проверка перехода со страницы ввода деталей заказа на Яндекс Дзен.')
+    def test_redirect_to_ya_dzen_from_order_rent_page(self, driver):
+        page = self.open_rent_page(driver)
+        self.assert_redirect_to_yandex(page, driver)
+
+    @allure.title('Проверка перехода с главной страницы по лого Самокат.')
+    def test_redirect_to_main_page_on_main_page(self, driver):
+        page = self.open_home_page(driver)
+        self.assert_redirect_to_main(page, driver)
+
+    @allure.title('Проверка перехода со страницы ввода персональных данных заказа по лого Самокат.')
+    def test_redirect_to_main_page_from_order_who_page(self, driver):
+        page = self.open_who_page(driver)
+        self.assert_redirect_to_main(page, driver)
+
+    @allure.title('Проверка перехода со страницы ввода деталей заказа по лого Самокат.')
+    def test_redirect_to_main_page_from_order_rent_page(self, driver):
+        page = self.open_rent_page(driver)
+        self.assert_redirect_to_main(page, driver)
+
+    def open_home_page(self, driver):
         home_page = MainPageScooter(driver)
         home_page.go_to_site()
         home_page.await_loaded()
-        # на форме заполнения персональных данных
-        if depth > 0:
-            home_page.click_order_button()
-            page = OrderPageWho(driver)
-            page.await_loaded()
-            # на форме заполнения деталей заказа
-            if depth > 1:
-                page.fill_personal_data('Имя', 'Фамилия', 'адрес', 'Курская', '89871234567')
-                page.click_next()
-                last_page = OrderPageRent(driver)
-                last_page.await_loaded()
+        return home_page
 
-        home_page.click_yandex()
+    def open_who_page(self, driver):
+        self.open_home_page(driver).click_order_button()
+        page = OrderPageWho(driver)
+        page.await_loaded()
+        return page
 
-        assert len(home_page.get_tabs_list()) == 2
-        home_page.switch_to_tab(1)
-        home_page.await_url(Constants.YA_DZEN_URL)
+    def open_rent_page(self, driver):
+        page = self.open_who_page(driver)
+        page.fill_personal_data('Имя', 'Фамилия', 'адрес', 'Курская', '89871234567')
+        page.click_next()
+        page = OrderPageRent(driver)
+        page.await_loaded()
+        return page
 
-    # переход по лого Самоката на разных этапах заказа
-    @pytest.mark.parametrize('depth', [0, 1, 2])
-    @allure.title('Проверка перехода со страниц самоката на главную страницу')
-    def test_redirect_to_main_page(self, driver, depth):
-        home_page = MainPageScooter(driver)
-        home_page.go_to_site()
-        home_page.await_loaded()
-        # на форме заполнения персональных данных
-        if depth > 0:
-            home_page.click_order_button()
-            page = OrderPageWho(driver)
-            page.await_loaded()
-            # на форме заполнения деталей заказа
-            if depth > 1:
-                page.fill_personal_data('Имя', 'Фамилия', 'адрес', 'Курская', '89871234567')
-                page.click_next()
-                last_page = OrderPageRent(driver)
-                last_page.await_loaded()
+    @staticmethod
+    def assert_redirect_to_yandex(page, driver):
+        page.click_yandex()
+        assert len(page.get_tabs_list()) == 2
+        page.switch_to_tab(1)
+        page.await_url(Constants.YA_DZEN_URL)
 
-        home_page.click_scooter()
+    @staticmethod
+    def assert_redirect_to_main(page, driver):
+        page.click_scooter()
 
         new_page = MainPageScooter(driver)
         new_page.await_loaded()
+
         assert len(new_page.get_tabs_list()) == 1
         assert new_page.get_current_url() == Constants.URL
